e1653418e3c53b542a48dc33b4e2873b87e61e0f
ptrkalm <pkalmuczak@gmail.com>
2022-04-27 21:01:20 +0200

    added static props

--- pages/index.js
+++ pages/index.js
@@ -3,7 +3,18 @@ import Link from 'next/link'
 import Layout, { siteTitle } from '../components/layout'
 import utilStyles from '../styles/utils.module.css'
 
-export default function Home() {
+import { getSortedPostsData } from '../lib/posts'
+
+export async function getStaticProps() {
+  const allPostsData = getSortedPostsData()
+  return {
+    props: {
+      allPostsData
+    }
+  }
+}
+
+export default function Home ({ allPostsData }) {
   return (
     <Layout home>
       <Head>
@@ -21,6 +32,20 @@ export default function Home() {
             <a>this page!</a>
           </Link>
       </h1>
+      </section>
+      <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
+        <h2 className={utilStyles.headingLg}>Blog</h2>
+        <ul className={utilStyles.list}>
+          {allPostsData.map(({ id, date, title }) => (
+            <li className={utilStyles.listItem} key={id}>
+              {title}
+              <br />
+              {id}
+              <br />
+              {date}
+            </li>
+          ))}
+        </ul>
       </section>
     </Layout>
   )
